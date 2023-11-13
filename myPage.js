60950e6b3eecf26183d51ba57ff2d577da9ca79e
dudxor4587 <68459414+dudxor4587@users.noreply.github.com>
2023-11-13 23:48:02 +0900

    myPage base html, css, js
    myPage의 기본 html과 css, js 작성

---
+++ myPage.js
@@ -0,0 +1,6 @@
+document.getElementById("logout").addEventListener("click", function(){
+    window.location.href = "mainPage.html";
+});
+document.getElementById("mainpage").addEventListener("click", function(){
+    window.location.href = "mainPage(login).html";
+});
