8013fca98783e8c237fedb3b333ce653b1440274
dudxor4587 <68459414+dudxor4587@users.noreply.github.com>
2023-11-13 22:20:56 +0900

    page move
    로그인/회원가입 버튼에 따라 페이지 이동

---
+++ mainPage.js
@@ -0,0 +1,6 @@
+document.getElementById("login").addEventListener("click", function(){
+    window.location.href = "loginPage.html";
+});
+document.getElementById("signin").addEventListener("click", function(){
+    window.location.href = "signinPage.html";
+});
