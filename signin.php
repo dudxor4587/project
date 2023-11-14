b5d4dbf257faedde46230b64c847e21fde1b1273
dudxor4587 <68459414+dudxor4587@users.noreply.github.com>
2023-11-14 15:44:54 +0900

    add Image at db
    Db에 유저 프로필사진 추가,
    회원가입시에 프로필 사진 업로드 기능 추가
    추가한 사진은 userImage 폴더에 저장됨

--- signin.php
+++ signin.php
@@ -15,12 +15,19 @@ $id = $_POST['id'];
 $pwd = $_POST['pwd'];
 $local = $_POST['local'];
 $nickname = $_POST['nickname'];
-
+if ($_FILES['image']['error'] === UPLOAD_ERR_OK) {
+    $id = $_POST['id'];
+    $uploadDir = 'userImages/';
+    $uploadFile = $uploadDir . basename($id . '.png');
+    move_uploaded_file($_FILES['image']['tmp_name'], $uploadFile);
+} else {
+    echo "파일 업로드에 오류가 발생했습니다: " . $_FILES['image']['error'];
+}
 // SQL 쿼리 실행하여 데이터베이스에 저장
-$sql = "INSERT INTO user_table (user_id, user_pw, location, name) VALUES ('$id', '$pwd', '$local', '$nickname')";
+$sql = "INSERT INTO user_table (user_id, user_pw, location, name, user_image) VALUES ('$id', '$pwd', '$local', '$nickname', '$id.png')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "회원가입이 완료되었습니다.";
+    
 } else {
     echo "오류: " . $sql . "<br>" . $conn->error;
 }
