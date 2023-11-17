2a98fcee8ab163e31d6503ee91f044c07c535cea
dudxor4587 <68459414+dudxor4587@users.noreply.github.com>
2023-11-17 20:44:10 +0900

    back-end
    진행상황 최신화

--- check_sale_info.php
+++ check_sale_info.php
@@ -6,8 +6,17 @@ $db = 'user_db';
 $user = 'root';
 $pass = '1234';
 
-// 클릭한 객체의 id 값과 로그인된 user_id 값 가져오기
+// 클릭한 객체의 id 값 가져오기
 $id = $_POST['id'];
+
+if (!isset($_SESSION['user_id'])) {
+    // 세션에 user_id가 없는 경우
+    $response = array('success' => false);
+    header('Content-Type: application/json'); // JSON 데이터를 반환한다는 헤더 설정
+    echo json_encode($response);
+    exit;
+}
+
 $user_id = $_SESSION['user_id'];
 $_SESSION['ID'] = $id;
 
