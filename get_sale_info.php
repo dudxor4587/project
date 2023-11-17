3cc6a6ec89bcab7bee690bca6c71160488e70319
dudxor4587 <68459414+dudxor4587@users.noreply.github.com>
2023-11-18 00:44:39 +0900

    back-end
    살려주세요

--- get_sale_info.php
+++ get_sale_info.php
@@ -6,7 +6,7 @@ $db = 'user_db';
 $user = 'root';
 $pass = '1234';
 
-// 로그인된 사용자의 ID 가져오기
+// 상품의 ID 가져오기
 $user_id = $_SESSION['ID'];
 
 try {
@@ -16,13 +16,27 @@ try {
     }
 
     // sale_table에서 해당 user_id에 해당하는 데이터 조회
-    $stmt = $conn->prepare("SELECT name, price, content, date FROM sale_table WHERE ID = ?");
+    $stmt = $conn->prepare("SELECT name, price, content, date, user_id FROM sale_table WHERE ID = ?");
     $stmt->bind_param('s', $user_id);
     $stmt->execute();
     $result = $stmt->get_result();
     // 데이터가 있는지 확인
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
+        
+        // user_table에서 user_id와 일치하는 행 조회
+        $stmt_user = $conn->prepare("SELECT name, user_image FROM user_table WHERE user_id = ?");
+        $stmt_user->bind_param('s', $row['user_id']);
+        $stmt_user->execute();
+        $result_user = $stmt_user->get_result();
+        
+        // 데이터가 있는지 확인
+        if ($result_user->num_rows > 0) {
+            $row_user = $result_user->fetch_assoc();
+            // 원하는 데이터를 $response에 추가
+            $response['user_name'] = $row_user['name'];
+            $response['user_image'] = $row_user['user_image'];
+        }
         
         // image_table에서 product_id와 일치하는 모든 행 조회
         $stmt_image = $conn->prepare("SELECT product_image FROM image_table WHERE product_id = ?");
@@ -37,30 +51,20 @@ try {
                 $product_images[] = $row_image['product_image'];
             }
             // 원하는 데이터를 $response에 추가
-            $response = array(
-                'success' => true,
-                'name' => $row['name'],
-                'price' => $row['price'],
-                'content' => $row['content'],
-                'date' => $row['date'],
-                'product_images' => $product_images
-            );
+            $response['product_images'] = $product_images;
         } else {
             // 이미지 데이터가 없을 경우
-            $response = array(
-                'success' => true,
-                'name' => $row['name'],
-                'price' => $row['price'],
-                'date' => $row['date'],
-                'content' => $row['content'],
-                'date' => $row['date'],
-                'product_images' => null
-            );
+            $response['product_images'] = null;
         }
+        
+        // 원하는 데이터를 $response에 추가
+        $response['success'] = true;
+        $response['name'] = $row['name'];
+        $response['price'] = $row['price'];
+        $response['content'] = $row['content'];
+        $response['date'] = $row['date'];
     } else {
-        $response = array(
-            'success' => false
-        );
+        $response['success'] = false;
     }
 
     // JSON 형식으로 데이터 반환
@@ -69,3 +73,4 @@ try {
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
 }
+?>
